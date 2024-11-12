b1fead1f1aa683249d6590fe15f55e3a13fe0cd5
jjricah <jericahagtual@gmail.com>
2024-11-12 23:52:16 +0800

    WFEwfe

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 function checkFullScreen() {
   const sidebar = document.querySelector(".sidebar");
   const fullScreenWidth = window.innerWidth;
-  const isWideScreen = fullScreenWidth >= 1400;
+  const isWideScreen = fullScreenWidth >= 1200;
 
   // Automatically close the sidebar on small screens by toggling the 'active' class
   if (!isWideScreen && sidebar.classList.contains("active")) {
